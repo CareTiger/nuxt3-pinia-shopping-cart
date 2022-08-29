f4da989e735ef2386759506ac289e1ef55325a37
CareTiger <currency@gmail.com>
2022-08-29 17:19:31 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ filtersList }}
+  </div>
+</template>
+
+// components/FilterMenu.vue
+<script>
+import { useFiltersStore } from '~/store/filters'
+
+export default defineComponent({
+  setup() {
+    const filtersStore = useFiltersStore()
+    const filtersList = filtersStore.getFiltersList
+
+    return { filtersList }
+  },
+})
+</script>
